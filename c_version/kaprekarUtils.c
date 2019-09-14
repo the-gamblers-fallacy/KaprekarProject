8a30c6dc7e49b1a1fadfae8c6cc5671b97dac111
Lucas Bigelow <lukejbigelow@gmail.com>
2019-09-13 20:38:20 -0500

    Fixed check for kaprekar numbers

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -33,7 +33,7 @@ int isKaprekar(int n) {
   long square = n * (long) n;
   //compute the number of digits of the square
   int numDigits = (int) log10(n) + 1;
-  long modulus = 0;
+  long modulus = 1;
   long first, second;
 
   //for each possible "split" of the square...
@@ -42,16 +42,16 @@ int isKaprekar(int n) {
     modulus *= 10;
 
     //split the square into two parts
-    first = square / modulus;
+    first = square % modulus;
     second = square % modulus;
 
     //test if the split makes a Kaprekar number
     //the second part cannot be zero and if
     //the sum of the two parts is equal to n, then it is
-    if(second > 0 && n == first + second) {
+    if(second != 0 && n == first + second) {
+      return 0;
+    } else {
       return 1;
     }
   }
-
-  return 0;
 }
